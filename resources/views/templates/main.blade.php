3a84eea11f82e9aeab92a6b86368202194a488de
johnny <jralvesoliveira15@gmail.com>
2021-01-27 22:48:00 -0300

    correcoes nas views e mix

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -7,24 +7,33 @@
 
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('fonts/feather/iconfont.css')) }}" />
+    <style>
+        .animate-scale {
+            transition: .3s;
+        }     
+        .animate-scale:hover {
+            transform: scale(1.05);
+        }
+    </style>
 </head>
-<body class="w-full">
-    <header class="w-full h-14 bg-red-600 px-24 flex justify-between items-center">
-        <a href="#" class="flex items-center px-7 h-full hover:bg-red-500 duration-200">
+<body class="w-full m-0 p-0">
+    <header class="w-full h-14 bg-grey-500 px-24 flex justify-between items-center absolute top-0">
+        <a href="#" class="flex items-center px-7 h-full hover:bg-opacity-50 duration-200">
             <img class="w-full" src="{{ asset('img/swed-logo-alt-sm.png') }}" alt="Swed logo" />
         </a>
         <div class="h-full flex items-center">
-            <a class="h-full px-7 py-4 text-white hover:bg-red-500 duration-200" href="#">Cidades Mapeadas</a>
-            <a class="h-full px-7 py-4 text-white hover:bg-red-500 duration-200" href="#">Quem somos</a>
-            <a class="h-full px-7 py-4 text-white hover:bg-red-500 duration-200" href="#">Sobre</a>
+            <a class="h-full px-7 py-4 font-semibold text-white hover:bg-red-500 animate-scale" href="#">Cidades Mapeadas</a>
+            <a class="h-full px-7 py-4 font-semibold text-white hover:bg-red-500 animate-scale" href="#">Quem somos</a>
+            <a class="h-full px-7 py-4 font-semibold text-white hover:bg-red-500 animate-scale" href="#">Sobre</a>
+            <a class="h-full px-7 py-4 font-semibold text-white hover:bg-red-500" href="#">Login</a>
         </div>
     </header>
 
     @yield('main')
 
-    <footer class="w-full h-40 bg-gray-900 px-24">
+    {{-- <footer class="w-full h-40 bg-gray-900 px-24">
 
-    </footer>
+    </footer> --}}
 
     @yield('script')
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,20 +6,25 @@
 
 @section('main')
     <main class="w-full">
-        <div style="background-image: url({{ asset('img/comercio-hero-image.jpg') }}); background-repeat: no-repeat; border: none; background-size: 100%"
-             class="w-full h-screen flex justify-center">
-            <div class="flex flex-col mt-10 w-full items-center">
+        <div style="background-image: url({{ asset('img/comercio-hero-image.jpg') }}); background-repeat: no-repeat; border: none; background-size: cover;"
+             class="w-full h-screen flex justify-center items-center">
+            <div class="flex flex-col w-full items-center">
                 <img src="{{ asset('img/swed-logo-lg.png') }}" alt="Swed logo" />
                 <form action="#" method="get" class="flex flex-row justify-center h-12 w-2/5 mt-10">
                     @csrf
                     <input type="text" name="category" id="category" class="h-full w-full px-4 outline-none rounded-l-full" placeholder="Que tipo de estabelecimento você está procurando hoje?" />
-                    <div class="h-full w-12 cursor-pointer bg-red-600 hover:bg-red-500 rounded-r-full">
-                        <i class="feather icon-search"></i>
+                    <div class="h-full w-12 cursor-pointer bg-red-600 hover:bg-red-500 rounded-r-full flex justify-center items-center">
+                        <i class="feather icon-search text-white text-xl -ml-2"></i>
                     </div>
                 </form>
                 <div class="flex flex-row mt-4 justify-between w-2/5 h-12">
-                    <a href="#" class="flex items-center justify-center text-white bg-red-600 hover:bg-red-500 h-full w-1/2 rounded-full duration-200">É Gerente? Clique aqui</a>
-                    <a href="#" class="flex items-center justify-center text-white bg-yellow-500 hover:bg-yellow-400 h-full w-1/2 rounded-full duration-200">É Cliente? Clique aqui</a>
+                    <fieldset class="pl-1">
+                        <input type="checkbox" name="buscar-aqui" id="buscar-aqui" class="form-checkbox h-5 w-5 text-red-600 outline-none rounded">
+                        <label for="buscar-aqui" class="text-gray-300">Buscar onde estou</label>
+                    </fieldset>
+                    
+                    {{-- <a href="#" class="flex items-center justify-center text-white bg-red-600 hover:bg-red-500 h-full w-1/2 rounded-full duration-200">É Gerente? Clique aqui</a>
+                    <a href="#" class="flex items-center justify-center text-white bg-yellow-500 hover:bg-yellow-400 h-full w-1/2 rounded-full duration-200">É Cliente? Clique aqui</a> --}}
                 </div>
             </div>
         </div>
