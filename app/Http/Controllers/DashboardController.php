51233813966ab72fe71f6d502c1a527f77e73de9
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-02-01 10:58:38 -0300

    Adiciona funções de retorno para view de login de cliente e gerente

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,4 +9,15 @@ class DashboardController extends Controller
     public function index() {
         return view('pages.dashboard');        
     }
+
+    public function loginCliente() {
+        $title = "Cliente";
+        return view('pages.login.index', compact('title'));
+    }
+
+    public function loginGerente() {
+        $title = "Gerente";
+        return view('pages.login.index', compact('title'));
+    }
+
 }
