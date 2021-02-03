ad9cac46b5250327b27fba64c3a6ad12ee0aa130
Alisso Felipe <alissonfelipe432@gmail.com>
2021-02-03 19:06:47 -0300

    Adicionando os Models do projeto

--- app/Models/Avaliation.php
+++ app/Models/Avaliation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Services extends Model
+class Avaliation extends Model
 {
     use HasFactory;
 }
