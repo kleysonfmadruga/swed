69a8db287acab5dce7cbedac18e879acdf8e1448
Alisso Felipe <alissonfelipe432@gmail.com>
2021-02-03 18:27:52 -0300

    Adicionando as migrations do projeto

---
+++ database/migrations/2021_02_03_183010_create_services_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->double('price', 10, 2);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
