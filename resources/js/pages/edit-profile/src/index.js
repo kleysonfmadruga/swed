0024a4d6d02c57ae7eecd7271b260f85134a0466
Kleyson Fernandes <kleysonfmadruga@gmail.com>
2021-02-18 22:05:33 -0300

    Adiciona interface e script para editar usuário

---
+++ resources/js/pages/edit-profile/src/index.js
@@ -0,0 +1,8 @@
+const imageToBase64 = (image) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+export {imageToBase64}
