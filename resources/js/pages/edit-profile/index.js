468b2eda8f1db17d9d962438e821db2e8d5740a3
Alisso Felipe <alissonfelipe432@gmail.com>
2021-03-03 13:20:39 -0300

    Adiciona scripts para gerenciar menu dropdown nas páginas

--- resources/js/pages/edit-profile/index.js
+++ resources/js/pages/edit-profile/index.js
@@ -3,11 +3,8 @@ import { hiddenDropdown, toggleDropdownVisibilityHandler, imageToBase64 } from '
 const photoInput = document.querySelector('input[name="photo"]');
 
 photoInput.addEventListener('input', async function(element) {
-    console.log(element)
     const base = await imageToBase64(photoInput.files[0])
     const profilePhoto = document.getElementById('profile-photo');
-
-    console.log(base)
 
     profilePhoto.src = base;
 });
--- resources/js/pages/view-profile/index.js
+++ resources/js/pages/view-profile/index.js
@@ -1,5 +1,4 @@
 import { hiddenDropdown, toggleDropdownVisibilityHandler } from '../utils/src/index.js';
-import { activeSelectContainer } from './src/index.js';
 
 window.onclick = (element) => {
     hiddenDropdown(element);
@@ -8,7 +7,3 @@ window.onclick = (element) => {
 document.getElementById('profile-dropdown-button').addEventListener('click', function(element) {
     toggleDropdownVisibilityHandler(element.target);
 });
-
-document.getElementById('search-other-cities').addEventListener('click', function(element) {
-    activeSelectContainer(element.target)
-})
